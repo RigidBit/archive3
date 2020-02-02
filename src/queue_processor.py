93a7dfa0ef72cf54a4003b879aa3f6905e2867fb
Jordan Mack <jordan.mack@brilliantnotion.com>
2020-02-02 14:29:18 -0800

    Perform minor cleanup.

--- src/queue_processor.py
+++ src/queue_processor.py
@@ -14,6 +14,7 @@ load_dotenv()
 queue = greenstalk.Client(host=os.getenv("GREENSTALK_HOST"), port=os.getenv("GREENSTALK_PORT"), use=os.getenv("GREENSTALK_TUBE_QUEUE"))
 
 def queue_screenshot(connection, id, url):
+	"""Queue a request to screenshot a URL."""
 	data = {"timestamp_queued": "NOW()"}
 	db.update_url_record(connection, id, data)
 	connection.commit()
@@ -22,14 +23,21 @@ def queue_screenshot(connection, id, url):
 
 while True:
 	connection = db.connect()
+
 	expired = db.get_expired_active_url_records(connection, int(os.getenv("ARCHIVE3_URL_EXPIRATION")), int(os.getenv("ARCHIVE3_URL_ERROR_RETRIES")))
+
+	# Always queue one random screenshot if nothing was expired.
 	if len(expired) == 0 and os.getenv("ARCHIVE3_QUEUE_RANDOM_URLS") == "true":
 		e = db.get_random_url_record(connection, int(os.getenv("ARCHIVE3_URL_ERROR_RETRIES")))
 		queue_screenshot(connection, e["id"], e["url"])
 		misc.log_message(f"""Queued Random Screenshot {e["id"]}: {e["url"]}""")
+
+	# Queue all expired.
 	for e in expired:
 		queue_screenshot(connection, e["id"], e["url"])
 		misc.log_message(f"""Queued Screenshot {e["id"]}: {e["url"]}""")
+
+	# Process all unprocessed submission records.
 	for p in db.get_unprocessed_submission_records(connection):
 		if db.get_processed_submission_record_count(connection)["count"] >= int(os.getenv("ARCHIVE3_PROCESSING_READY_LIMIT")):
 			break
@@ -44,5 +52,6 @@ while True:
 			payload = {"type": "preview", "id": p["id"], "url": p["url"]}
 			queue.put(json.dumps(payload), ttr=int(os.getenv("ARCHIVE3_PROCESSING_TTR")))
 			misc.log_message(f"""Queued Preview {p["id"]}: {p["url"]}""")
+
 	connection.close()
 	time.sleep(int(os.getenv("ARCHIVE3_QUEUE_PROCESSOR_SLEEP")))
